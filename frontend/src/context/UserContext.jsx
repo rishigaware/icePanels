33e5863452fb43947ee0a6b51488140e584b5f54
Rushikesh Gaware <rushikesh@Rushikeshs-MacBook-Air.local>
2024-12-24 00:11:01 +0530

    url update after changes

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -6,8 +6,8 @@ const UserContext = createContext();
 // UserProvider component to wrap the app and provide user data
 export const UserProvider = ({ children }) => {
   // State to manage the user object
-  // const [url, setUrl] = useState("https://betting-accounts-backend.onrender.com");
-  const [url, setUrl] = useState("http://localhost:3000");
+  const [url, setUrl] = useState("https://betting-accounts-backend.onrender.com");
+  // const [url, setUrl] = useState("http://localhost:3000");
   const [user, setUser] = useState(() => {
     const storedUser = localStorage.getItem('user');
     try {
