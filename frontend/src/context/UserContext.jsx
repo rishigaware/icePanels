a23b4fccd2865d8be24220269d9228fa0ec81a15
Rushikesh Gaware <rushikesh@Rushikeshs-MacBook-Air.local>
2024-12-24 10:48:26 +0530

    Sai render url added in frontend

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -5,8 +5,8 @@ const UserContext = createContext();
 
 // UserProvider component to wrap the app and provide user data
 export const UserProvider = ({ children }) => {
-  // State to manage the user object
-  const [url, setUrl] = useState("https://betting-accounts-backend.onrender.com");
+  // State to manage the user object 
+  const [url, setUrl] = useState("https://betting-accounts-manager.onrender.com");
   // const [url, setUrl] = useState("http://localhost:3000");
   const [user, setUser] = useState(() => {
     const storedUser = localStorage.getItem('user');
